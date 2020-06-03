4777b37ba7e76a9dd49e614a4c7dd17ebfa9824a
Dimitar Natskin <dimitar.natskin@emerchantpay.com>
2020-06-03 21:30:58 +0300

    1.5.2

--- ext/modules/payment/emerchantpay/method_base.php
+++ ext/modules/payment/emerchantpay/method_base.php
@@ -46,7 +46,7 @@ abstract class emerchantpay_method_base extends emerchantpay_base
      * Return Module Version
      * @var string
      */
-    public $version         = "1.5.1";
+    public $version         = '1.5.2';
     /**
      * Return Module Version
      * @var string
